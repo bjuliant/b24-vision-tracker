5f3a342ff3ee10c31ee093bb10bba50f5a70c1dd
bjuliant <bjuliant@gmail.com>
2026-07-09 14:00:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.B24_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://owhfugfkwsgzlnsocsob.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_MIxeK53ECHlL2ZlgbVwArQ_dd1YsVNt",
   MAP_ID: "b24-main"
 };
